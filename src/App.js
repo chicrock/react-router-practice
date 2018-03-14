ed7a0a81158afcc036fb0f9382dbccb13cbb4c74
ChicRock <jungsungrak@gmail.com>
2018-03-14 22:01:40 +0900

    Add Basic route usage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { Route } from 'react-router-dom';
+
 const App = () => (
     <div>
         <Header />
@@ -9,7 +11,19 @@ const App = () => (
 const Header = () => (
     <header>
         <h1>My Contacts</h1>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/contacts" component={Contacts} />
     </header>
+);
+
+const Welcome = () => <h1>Welcome to the contact app!!</h1>;
+
+const Contacts = () => (
+    <ul>
+        <li>Rock</li>
+        <li>Lynn</li>
+        <li>Nicolas</li>
+    </ul>
 );
 
 export default App;
